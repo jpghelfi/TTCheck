f04fc96513441b8f2259720559cb8367cf5725fe
jp <juan.ghelfi@tictapps.com>
2018-06-07 17:41:59 -0300

    travis 100 ci

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -6,7 +6,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $'123456' -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P '123456' -T /usr/bin/codesign
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
