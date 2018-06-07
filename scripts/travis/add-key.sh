1b8351206376089423a3f14bd3d30ca82bc9fcc1
jp <juan.ghelfi@tictapps.com>
2018-06-07 17:37:52 -0300

    travis 80 ci

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
 security create-keychain -p travis ios-build.keychain
-security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $123456 -T /usr/bin/codesign
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $'123456' -T /usr/bin/codesign
+
+# Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
